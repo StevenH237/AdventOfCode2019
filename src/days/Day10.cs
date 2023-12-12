4ae59d74f58e74a19c89b403f212f9d7233561b2
Steven <github@steven-h.net>
2023-12-11 22:57:31 -0500

    Day 10 part 2!

--- src/days/Day10.cs
+++ src/days/Day10.cs
@@ -1,15 +1,17 @@
+using Nixill.Collections;
 using Nixill.Utils;
 
 public class Day10
 {
   Dictionary<(int X, int Y), D10Asteroid> Asteroids = new();
-  int Width = 0;
-  int Height = 0;
+  D10Asteroid MonitoringStation = null;
+  D10Asteroid TwoHundredthVictim = null;
 
-  public Day10(StreamReader input)
+  public Day10(string fname, StreamReader input)
   {
     string[] lines = input.GetAllLines();
 
+    // Find all the asteroids
     foreach ((string line, int y) in lines.WithIndex())
     {
       foreach ((char c, int x) in line.WithIndex())
@@ -22,9 +24,7 @@ public class Day10
       }
     }
 
-    Width = lines[0].Length;
-    Height = lines.Length;
-
+    // Store the asteroids in a list by their angle
     foreach (IEnumerable<D10Asteroid> asteroidPair in Asteroids.Values.Permutations(2))
     {
       D10Asteroid source = asteroidPair.First();
@@ -34,17 +34,38 @@ public class Day10
       vec = GetCoprimeVector(vec);
       var los = source.LinesOfSight;
 
-      if (los.ContainsKey(vec))
-      {
-        D10Asteroid other = los[vec];
-        if (GetDistance(source, target) < GetDistance(source, other))
-          los[vec] = target;
-      }
-      else
-      {
-        los[vec] = target;
-      }
+      los[vec].Add(target);
     }
+
+    // Get the monitoring station on the asteroid with the best line of
+    // sight
+    MonitoringStation = Asteroids
+      .OrderByDescending(x => x.Value.LinesOfSight.Count)
+      .First().Value;
+
+    // Get the list of lines of sight, sorted by relative angle (luckily,
+    // the key of that dictionary is in fact the angle), with each sublist
+    // sorted by distance.
+    List<List<D10Asteroid>> angleSortedList = MonitoringStation
+      .LinesOfSight
+      .OrderBy(x => x.Key, D10AngleComparer.Instance)
+      .Select(x => x.Value
+        .OrderBy(x => GetDistance(MonitoringStation, x))
+        .ToList()
+      ).ToList();
+
+    int shots = 0;
+    D10Asteroid shot = null;
+
+    while (shots < 200 && angleSortedList.Count > 0)
+    {
+      List<D10Asteroid> nextAngle = angleSortedList.Pop();
+      shot = nextAngle.Pop();
+      if (nextAngle.Count > 0) angleSortedList.Add(nextAngle);
+      shots++;
+    }
+
+    TwoHundredthVictim = shot;
   }
 
   public static (int X, int Y) GetDisplacement(D10Asteroid from, D10Asteroid to)
@@ -71,17 +92,23 @@ public class Day10
   static Day10 Get(string fname, StreamReader input)
   {
     if (!results.ContainsKey(fname))
-      results[fname] = new(input);
+      results[fname] = new(fname, input);
     return results[fname];
   }
 
   public static string Part1(string fname, StreamReader input)
     => Get(fname, input)
-      .Asteroids
-      .Select(e => e.Value.LinesOfSight.Count)
-      .OrderDescending()
-      .First()
+      .MonitoringStation
+      .LinesOfSight
+      .Count
       .ToString();
+
+  public static string Part2(string fname, StreamReader input)
+  {
+    D10Asteroid victim = Get(fname, input).TwoHundredthVictim;
+
+    return (victim.X * 100 + victim.Y).ToString();
+  }
 }
 
 public class D10Asteroid
@@ -89,5 +116,55 @@ public class D10Asteroid
   public int X { get; internal set; }
   public int Y { get; internal set; }
 
-  internal Dictionary<(int X, int Y), D10Asteroid> LinesOfSight = new();
+  internal DictionaryGenerator<(int X, int Y), List<D10Asteroid>> LinesOfSight
+    = new(new EmptyConstructorGenerator<(int X, int Y), List<D10Asteroid>>());
+}
+
+public class D10AngleComparer : IComparer<(int X, int Y)>
+{
+  int GetAreaNumber((int X, int Y) angle)
+  {
+    int area = 0;
+    (int x, int y) = angle;
+
+    if (x < 0)
+    {
+      area += 8;
+      (x, y) = (-x, -y);
+    }
+
+    if (y >= 0)
+    {
+      area += 4;
+      (x, y) = (y, -x);
+    }
+
+    if (x == 0 && y < 0) return area;
+    if (x > 0 && y == 0) return area + 4;
+    if (-y > x) return area + 1;
+    if (-y == x) return area + 2;
+    if (-y < x) return area + 3;
+
+    return 0;
+  }
+
+  public int Compare((int X, int Y) l, (int X, int Y) r)
+  {
+    int areaL = GetAreaNumber(l);
+    int areaR = GetAreaNumber(r);
+    if (areaL != areaR) return areaL - areaR;
+
+    int clx = Math.Abs(l.X);
+    int crx = Math.Abs(r.X);
+
+    l = (l.X * crx, l.Y * crx);
+    r = (r.X * clx, r.Y * clx);
+
+    if ((l.Y > r.Y) != (areaL > 8)) return 1;
+    return -1;
+  }
+
+  public static readonly D10AngleComparer Instance = new();
+
+  private D10AngleComparer() { }
 }
